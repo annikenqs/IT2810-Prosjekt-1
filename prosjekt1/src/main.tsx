7ea7b29ba3678a4f4e03437131d99c57fb80fe84
Anniken Strand <annikeqs@stud.ntnu.no>
2024-09-09 16:32:56 +0200

    Create: Create front page

--- prosjekt1/src/main.tsx
+++ prosjekt1/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import JokeCard from "./components/JokeCard/JokeCard.tsx";
+import "./components/QuoteCard/QuoteCard.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
+		<JokeCard
+			quote="The only limit to our realization of tomorrow is our doubts of today."
+			author="Franklin D. Roosevelt"
+		/>
 	</StrictMode>,
 );
