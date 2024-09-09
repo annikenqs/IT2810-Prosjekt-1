45c262e7e82f61e834f0dd535e01ac458527c0ce
Anna Farestrand <annatfa@stud.ntnu.no>
2024-09-09 17:38:48 +0200

    Fix: Un-updated import, from Quote to Joke

--- prosjekt1/src/main.tsx
+++ prosjekt1/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import JokeCard from "./components/JokeCard/JokeCard.tsx";
-import "./components/QuoteCard/QuoteCard.css";
+import "./components/JokeCard/JokeCard.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
