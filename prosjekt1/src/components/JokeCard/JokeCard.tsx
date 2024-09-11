904202119ed3cef50a67a9841da61b92da99c35d
Anna Farestrand <annatfa@stud.ntnu.no>
2024-09-11 13:48:24 +0200

    Add: Make button to save favorites

--- prosjekt1/src/components/JokeCard/JokeCard.tsx
+++ prosjekt1/src/components/JokeCard/JokeCard.tsx
@@ -4,6 +4,7 @@ import { useSpookyJoke } from "../../restAPI/jokesAPI";
 
 function JokeCard() {
 	const [joke, setJoke] = useState<string>("Loading...");
+	const [isFavorite, setIsFavorite] = useState<boolean>(false);
 	const { data: jokeData, error, isLoading} = useSpookyJoke();
 	
 	useEffect(() => {
@@ -22,15 +23,21 @@ function JokeCard() {
 		}
 	}, [isLoading, error, jokeData]);
 
+	const handleFavoriteClick = () => {
+		setIsFavorite(!isFavorite); // Toggle the favorite state
+	  }; // TODO: Implement favorite functionality
 
 	return (
-		<>
-			<div className="card" role="figure">
-				<h3>Random Joke</h3>
-				<p>- {joke}</p>
-			</div>
-		</>
-	);
+		<div className="card" role="figure">
+		  <h3>Random Joke</h3>
+		  <p>- {joke}</p>
+
+		  {/* Star: to favorite a joke */}
+		  <button onClick={handleFavoriteClick} className="favorite-button">
+			{isFavorite ? "★" : "☆"}
+		  </button>
+		</div>
+	  );
 }
 
 export default JokeCard;
