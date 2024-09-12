85684f40dde7f89c917be0d3aa30f3f229981cb9
Anna Farestrand <annatfa@stud.ntnu.no>
2024-09-12 12:49:28 +0200

    Add: Added favoritefunctionality to homepage

--- prosjekt1/src/App.tsx
+++ prosjekt1/src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import JokeCard from "./components/JokeCard/JokeCard";
+import FavoritePage from "./components/FavoritePage/FavoritePage";
 
 function App() {
+	const [showFavorites, setShowFavorites] = useState(false); // State to track which page to show
+
+	const handleToggleClick = () => {
+		setShowFavorites((prevShowFavorites) => !prevShowFavorites); // Toggle between true and false
+	};
+
 	return (
 		<>
 			<h1>The Giggle Garden</h1>
-			<JokeCard />
+			{/* Button text changes based on showFavorites state */}
+			<button className="button" onClick={handleToggleClick}>
+				{showFavorites ? "Back" : "Favorites"}
+			</button>
+			
+			{/* Conditionally render FavoritePage or JokeCard based on state */}
+			{showFavorites ? <FavoritePage /> : <JokeCard />}
 		</>
 	);
 }
