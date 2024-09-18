6a3557bf24fabeac58497886199a18c33d99881e
Anna Farestrand <annatfa@stud.ntnu.no>
2024-09-18 13:46:14 +0200

    Add: Setup for tests with Vitest

---
+++ prosjekt1/src/test/setup.ts
@@ -0,0 +1,1 @@
+import '@testing-library/jest-dom'; // Custom DOM matchers for Vitest
---
+++ prosjekt1/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+      globals: true, // Enable Vitest globals like `expect`
+      setupFiles: './src/test/setup.ts', // Path to your setup file
+        environment: 'jsdom',  // JSDOM is the default environment
+    },
+  });
